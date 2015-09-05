b5d4c5cc0d1c9cc333e592053a2393cfbad6b973
Maik Hummel <yo@beevelop.com>
2015-09-05 13:09:21 +0200

    nodeled example

---
+++ nodeled/simpletest.js
@@ -0,0 +1,11 @@
+var gpio = require("pi-gpio");
+
+var pinNum = 0;
+
+gpio.open(pinNum, "output", function(err) {     // Open pin 16 for output
+    console.log('Opened Pin '+pinNum);
+    gpio.write(pinNum, 1, function() {          // Set pin 16 high (1)
+        console.log('Wrote Pin '+pinNum);
+        gpio.close(pinNum);                     // Close pin 16
+    });
+});
