0b538b2da805d0636df46b58e396105885809820
Maik Hummel <yo@beevelop.com>
2015-09-06 10:03:35 +0200

    add travistatus example

---
+++ travistatus/travistatus.js
@@ -0,0 +1,40 @@
+var Travis = require('travis-ci');
+var travis = new Travis({
+    version: '2.0.0'
+});
+
+var failed = [];
+var passed = [];
+var interv = null;
+travis.repos('beevelop').get(function (err, res) {
+    var repos = res.repos;
+    repos.forEach(function (repo) {
+        if (repo.last_build_state === 'failed') {
+            failed.push(repo);
+        }
+
+        if (repo.last_build_state === 'passed') {
+            passed.push(repo);
+        }
+    });
+
+    console.log('Failed: '+failed.length);
+    console.log('Passed: '+passed.length);
+
+    if (interv === null) {
+        interv = setInterval(showRepos, 2000);
+    }
+});
+
+var i = 0;
+function showRepos() {
+    var repos = failed.concat(passed);
+    i = i + 1;
+
+    showRepo(repos[i % repos.length]);
+}
+
+function showRepo(repo) {
+    var repoName = repo.slug.split('/').slice(-1)[0];
+    console.log(repo.last_build_state+': '+repoName);
+}
